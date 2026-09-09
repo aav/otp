0763ec1f74db06cc9643beb8f51b58ad20e95b0e
Alexey Aristov <aav@acm.org>
2026-09-09 20:05:04 +0200

    Fix use-after-free when several trace sessions trace garbage collection

--- erts/emulator/beam/erl_trace.c
+++ erts/emulator/beam/erl_trace.c
@@ -1423,6 +1423,9 @@ void save_calls(Process *p, const Export *e)
     }
 }
 
+static void
+trace_gc_session(Process *p, Eterm what, Uint size, Eterm msg, ErtsTracerRef*);
+
 /* Sends trace message:
  *    {trace_ts, Pid, What, Msg, Timestamp}
  * or {trace, Pid, What, Msg}
@@ -1443,34 +1446,43 @@ trace_gc(Process *p, Eterm what, Uint size, Eterm msg)
     ErtsTracerRef *next_ref;
 
     for (ref = p->common.tracee.first_ref; ref; ref = next_ref) {
-        ErtsTracerNif *tnif = NULL;
-
         next_ref = ref->next;
-        if (IS_SESSION_TRACED_FL(ref, F_TRACE_GC)
-            && is_tracer_ref_enabled(p, ERTS_PROC_LOCK_MAIN, &p->common, ref,
-                                     &tnif, TRACE_FUN_E_GC, what)) {
-            Eterm* o_hp = NULL;
-            Eterm* hp;
-            Uint sz = 0;
-            Eterm tup;
-
-            if (is_non_value(msg)) {
-
-                (void) erts_process_gc_info(p, &sz, NULL, 0, 0);
-                o_hp = hp = erts_alloc(ERTS_ALC_T_TMP, (sz + 3 + 2) * sizeof(Eterm));
-
-                msg = erts_process_gc_info(p, NULL, &hp, 0, 0);
-                tup = TUPLE2(hp, am_wordsize, make_small(size)); hp += 3;
-                msg = CONS(hp, tup, msg); hp += 2;
-            }
-
-            send_to_tracer_nif(p, &p->common, ref, p->common.id, tnif, TRACE_FUN_T_GC,
-                               what, msg, THE_NON_VALUE, am_true);
-            if (o_hp)
-                erts_free(ERTS_ALC_T_TMP, o_hp);
+        if (IS_SESSION_TRACED_FL(ref, F_TRACE_GC)) {
+            trace_gc_session(p, what, size, msg, ref);
         }
     }
     erts_thr_progress_unmanaged_continue(dhndl);
+}
+
+/* 'msg' is passed by value, so each session builds its own message. */
+static void
+trace_gc_session(Process *p, Eterm what, Uint size, Eterm msg,
+                 ErtsTracerRef *ref)
+{
+    ErtsTracerNif *tnif = NULL;
+    Eterm* o_hp = NULL;
+    Eterm* hp;
+    Uint sz = 0;
+    Eterm tup;
+
+    if (!is_tracer_ref_enabled(p, ERTS_PROC_LOCK_MAIN, &p->common, ref,
+                               &tnif, TRACE_FUN_E_GC, what))
+        return;
+
+    if (is_non_value(msg)) {
+
+        (void) erts_process_gc_info(p, &sz, NULL, 0, 0);
+        o_hp = hp = erts_alloc(ERTS_ALC_T_TMP, (sz + 3 + 2) * sizeof(Eterm));
+
+        msg = erts_process_gc_info(p, NULL, &hp, 0, 0);
+        tup = TUPLE2(hp, am_wordsize, make_small(size)); hp += 3;
+        msg = CONS(hp, tup, msg); hp += 2;
+    }
+
+    send_to_tracer_nif(p, &p->common, ref, p->common.id, tnif, TRACE_FUN_T_GC,
+                       what, msg, THE_NON_VALUE, am_true);
+    if (o_hp)
+        erts_free(ERTS_ALC_T_TMP, o_hp);
 }
 
 static void
